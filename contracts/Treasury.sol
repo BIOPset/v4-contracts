3adf7ac7af42a8838893c59598758d9286001292
shalaquiana <75330317+shalaquiana@users.noreply.github.com>
2021-05-05 22:29:13 -0400

    Update Treasury.sol

--- contracts/Treasury.sol
+++ contracts/Treasury.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.6.6;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Treasury {
     using SafeMath for uint256;
@@ -21,12 +22,21 @@ contract Treasury {
     }
 
     function sendFunds(uint256 amount, address payable destination) public onlyDGov {
-        require(address(this).balance <= amount, "not enough to send");
+        require(address(this).balance >= amount, "not enough to send");
         destination.send(amount);
         emit FundsSent(amount, destination);
     }
 
+    function sendERC20Funds(address token, uint256 amount, address payable destination) public onlyDGov {
+        ERC20 tk = ERC20(token);
+        uint256 balance = tk.balanceOf(address(this));
+        require(balance >= amount, "not enough to send");
+        tk.transfer(destination, amount);
+        emit ERC20FundsSent(token, amount, destination);
+    }
+
 
     event FundsSent(uint256 amount, address destination);
+    event ERC20FundsSent(address token, uint256 amount, address destination);
 
 }
