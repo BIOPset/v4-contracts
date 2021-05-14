550f655425dd9d3fbbd3337866be13280eed1123
shalaquiana <75330317+shalaquiana@users.noreply.github.com>
2021-05-14 16:40:22 -0400

    Create IRateCalc.sol

---
+++ contracts/interfaces/IRateCalc.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.6.6;
+
+interface IRateCalc {
+    /**
+     * @notice Returns the rate to pay out for a given amount
+     * @param amount the bet amount to calc a payout for
+     * @param l the current amount locked
+     * @param t time for the option
+     * @param k true for call false for put
+     * @param s number of open options in this direction
+     * @return profit total possible profit amount
+     *
+     */
+    function rate(uint256 amount, uint256 l, uint256 t, bool k, uint256 s) external view returns (uint256);
+
+}
