43fb8e0ed69c8351a49c8c5c06ebc3d786b01a85
shalaquiana <75330317+shalaquiana@users.noreply.github.com>
2021-05-13 22:57:32 -0400

    Update AdaptiveRateCalc.sol

--- contracts/AdaptiveRateCalc.sol
+++ contracts/AdaptiveRateCalc.sol
@@ -9,17 +9,16 @@ contract AdaptiveRateCalc is IRCD {
      /**
      * @notice Calculates maximum option buyer profit
      * @param amount Option amount
-     * @param maxAvailable the total pooled ETH unlocked and available to bet
      * @param l the current amount of locked ETH
      * @param t time for the option
      * @param k true for call false for put
      * @param s stack, how many open options there are in this direction
      * @return profit total possible profit amount
      */
-    function rate(uint256 amount, uint256 maxAvailable, uint256 l, uint256 t, bool k, uint256 s) external view override returns (uint256)  {
+    function rate(uint256 amount, uint256 l, uint256 t, bool k, uint256 s) external view override returns (uint256)  {
         require(s > 0, "invalid stack");
 
-        if (amount < maxAvailable.sub(l).div(1000)) {
+        if (amount < address(msg.sender).balance.sub(l).div(1000)) {
             return amount.mul(2);
         } else {
            //bottom 1.01x
