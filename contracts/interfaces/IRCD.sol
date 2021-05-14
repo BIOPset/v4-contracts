254a5d6dec8e097040f33e035c9e6c69061ce3f4
shalaquiana <75330317+shalaquiana@users.noreply.github.com>
2021-05-13 22:57:51 -0400

    Update IRCD.sol

--- contracts/interfaces/IRCD.sol
+++ contracts/interfaces/IRCD.sol
@@ -4,7 +4,6 @@ interface IRCD {
     /**
      * @notice Returns the rate to pay out for a given amount
      * @param amount the bet amount to calc a payout for
-     * @param maxAvailable the total pooled ETH unlocked and available to bet
      * @param l the current amount locked
      * @param t time for the option
      * @param k true for call false for put
@@ -12,6 +11,6 @@ interface IRCD {
      * @return profit total possible profit amount
      *
      */
-    function rate(uint256 amount, uint256 maxAvailable, uint256 l, uint256 t, bool k, uint256 s) external view returns (uint256);
+    function rate(uint256 amount, uint256 l, uint256 t, bool k, uint256 s) external view returns (uint256);
 
 }
