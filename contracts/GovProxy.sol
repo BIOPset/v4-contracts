fe407151e648a7f61ba52cf7e0b821405c722eda
munair <munair@gmail.com>
2021-05-18 08:19:32 +0900

    1% goes to the treasury.

--- contracts/GovProxy.sol
+++ contracts/GovProxy.sol
@@ -4,67 +4,51 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /**
- * @title Binary Options Gov Proxy
+ * @title Binary Options Treasury Proxy
  * @author github.com/BIOPset
- * @dev intermediary holds funds generated from bets until they are transfered to gov
- * @notice intermediary holds funds generated from bets until they are transfered to gov
+ * @dev intermediary holds funds generated from bets until they are transferred to the treasury.
+ * @notice intermediary holds funds generated from bets until they are transferred to the treasury.
  * Biop
  */
 contract GovProxy {
     using SafeMath for uint256;
-    address payable public dgov;
     address payable public treasury;
-    uint256 public tFee = 100;//1%. This fee is for the transfer caller
-    uint256 public treasuryFee = 10;//10%
+    uint256 public treasuryFee = 1;//1%
 
     constructor() public {
-        dgov = msg.sender;
+        governance = msg.sender;
     }
-      
 
-    modifier onlyDGov() {
-        require(dgov == msg.sender, "Ownable: caller is not the dgov");
+    //ensure that this contract is only callable by governance (the Settlement DAO).
+    modifier onlyGovernance() {
+        require(governance == msg.sender, "Ownable: caller is not the governance");
         _;
     }
 
-    function updateTFee(uint256 new_) external onlyDGov {
-        tFee = new_;
+    //change the address of governance (the Settlement DAO) if required.
+    function updateGovernance(address payable new_) external onlyGovernance {
+        governance = new_;
     }
 
-    function updateDGov(address payable new_) external onlyDGov {
-        dgov = new_;
-    }
-
-    function updateTreasuryAmount(uint256 new_) external onlyDGov {
+    function updateTreasuryAmount(uint256 new_) external onlyGovernance {
         treasuryFee = new_;
     }
 
-    function updateTreasury(address payable new_) external onlyDGov {
+    //change the address of the treasury if required.
+    function updateTreasury(address payable new_) external onlyGovernance {
         treasury = new_;
     }
 
     /**
-    * @dev transfer ETH or ERC20 tokens to the treasury or to the DGov directly for stakers to claim
+    * @dev transfer ETH or ERC20 tokens to the treasury.
     */
-    function transferToGov() external onlyDGov returns(uint256){
+    function transferToGov() external onlyGovernance returns(uint256){
             require(address(this).balance > 0, "Nothing to transfer");
-            uint256 fee = address(this).balance.div(tFee);
-            uint256 tT;
-            if (treasuryFee != 0) {
-                //if treasury fee is not zero then calculate it
-                tT = (address(this).balance.sub(fee)).div(treasuryFee);
-                treasury.send(tT);
+            uint256 fee = address(this).balance.div(100).mul(treasuryFee);
+            if (fee != 0) {
+                //if treasury fee is not zero send it to the treasury
+                treasury.send(fee);
             }
-            uint256 fG = 0;//amount for gov direct is zero by default
-            if (treasuryFee > 1) {
-                //if treasury fee is not 100% send some direct to gov
-                uint256 tG = address(this).balance.sub(fee);
-                dgov.send(tG);
-                fG = tG;
-            }
-            tx.origin.send(fee);
-            return fG;
-       
     }
 
     fallback () external payable {}
