6f32bebb09e5998e1f89e8ffbc8fbafe3801d956
shalaquiana <75330317+shalaquiana@users.noreply.github.com>
2021-05-21 00:00:16 -0400

    Update RateCalc.sol

--- contracts/RateCalc.sol
+++ contracts/RateCalc.sol
@@ -18,20 +18,6 @@ contract RateCalc is IRateCalc {
      */
     function rate(uint256 amount,  uint256 l, uint256 t, bool k, uint256 s, uint256 tP) external view override returns (uint256)  {
         require(false, "Don't use this ratecalc its for format purpose only");
-        /* 
-        uint256 oneTenth = amount.div(10);
-        uint256 halfMax = msg.sender.balance.div(2);
-        if (amount > halfMax) {
-            return amount.mul(2).add(oneTenth).add(oneTenth);
-        } else {
-            if(oneTenth > 0) {
-                return amount.mul(2).sub(oneTenth);
-            } else {
-                uint256 oneThird = amount.div(4);
-                require(oneThird > 0, "invalid bet amount");
-                return amount.mul(2).sub(oneThird);
-            }
-        } */
         
     }
 }
