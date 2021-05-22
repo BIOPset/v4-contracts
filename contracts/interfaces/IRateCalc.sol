bb9591e4d9773693a4bfd86f16f111bd8dde6ca4
shalaquiana <75330317+shalaquiana@users.noreply.github.com>
2021-05-22 16:26:03 -0400

    Update IRateCalc.sol

--- contracts/interfaces/IRateCalc.sol
+++ contracts/interfaces/IRateCalc.sol
@@ -7,11 +7,12 @@ interface IRateCalc {
      * @param l the current amount locked
      * @param t time for the option
      * @param k true for call false for put
-     * @param s number of open options in this direction
+     * @param oC number of open call options
+     * @param oP number of open put options
      * @param tP the amount of tokens stored currently in the pool the option be created on
      * @return profit total possible profit amount
      *
      */
-    function rate(uint256 amount, uint256 l, uint256 t, bool k, uint256 s, uint256 tP) external view returns (uint256);
+    function rate(uint256 amount, uint256 l, uint256 t, bool k, uint256 oC, uint256 oP, uint256 tP) external view returns (uint256);
 
 }
