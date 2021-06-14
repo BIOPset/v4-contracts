299e3fe85ed66bd0be418c4c097de6b65ade0c60
Sha <shalaquiana@protonmail.com>
2021-06-14 19:54:46 -0400

    DAT-01C simplify return statements

--- contracts/DelegatedAccessTiers.sol
+++ contracts/DelegatedAccessTiers.sol
@@ -7,33 +7,21 @@ contract DelegatedAccessTiers is IAccessTiers {
     using SafeMath for uint256;
     function tier1(uint256 power, uint256 total) external override returns (bool) {
         uint256 half = total.div(2);
-        if (power >= half) {
-            return true;
-        }
-        return false;
+        return power >= half;
     }
 
     function tier2(uint256 power, uint256 total) external override returns (bool) {
         uint256 twothirds = total.mul(2).div(3);
-        if (power >= twothirds) {
-            return true;
-        }
-        return false;
+        return power >= twothirds;
     }
 
     function tier3(uint256 power, uint256 total) external override returns (bool) {
         uint256 threeQuarters = total.mul(3).div(4);
-        if (power >= threeQuarters) {
-            return true;
-        }
-        return false;
+        return power >= threeQuarters;
     }
 
     function tier4(uint256 power, uint256 total) external override returns (bool) {
         uint256 ninety = total.mul(9).div(10);
-        if (power >= ninety) {
-            return true;
-        }
-        return false;
+        return power >= ninety;
     }
 }
