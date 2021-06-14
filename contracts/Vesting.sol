2070f8e2bf181002b8279cf3a6a715567ea9a0aa
Sha <shalaquiana@protonmail.com>
2021-06-14 17:57:01 -0400

    VES-01M replace ERC20 with SafeERC20 and update transfer/transferFrom usages to safe equivalents

--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -1,13 +1,15 @@
 pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 
 
 contract Vesting{
 
     using SafeMath for uint256;
+    using SafeERC20 for IERC20;
     address public tokenAddress;
     address payable claimant;
     uint256 public period;
@@ -37,8 +39,8 @@ contract Vesting{
 
     function start(uint256 amount, uint256 period_) public {
         require(started == false, "already started");
-        ERC20 token = ERC20(tokenAddress);
-        token.transferFrom(msg.sender, address(this), amount);
+        IERC20 token = IERC20(tokenAddress);
+        token.safeTransferFrom(msg.sender, address(this), amount);
         started = true;
         period = period_;
         total = amount;
@@ -47,18 +49,18 @@ contract Vesting{
 
     function collect() public onlyClaimant returns(uint256) {
         uint256 elapsed = block.timestamp.sub(startTime);
-        ERC20 token = ERC20(tokenAddress);
+        IERC20 token = IERC20(tokenAddress);
         if (elapsed > period) {
             //vesting totally complete
             uint256 amount = total.sub(claimed);
             claimed = total;
-            token.transfer(claimant, amount);
+            token.safeTransfer(claimant, amount);
         } else {
             uint256 perComplete = period.div(elapsed);
             uint256 amount = total.div(perComplete);
             amount = amount.sub(claimed);
             claimed = claimed.add(amount);
-            token.transfer(claimant, amount);
+            token.safeTransfer(claimant, amount);
             return amount;
         }
     }
