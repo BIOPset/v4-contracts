0a4083e1063e3028c64f27d03f9473383f2c2e1e
Sha <shalaquiana@protonmail.com>
2021-06-14 20:44:51 -0400

    BRC-02M made thresholds inclusive

--- contracts/BasicRateCalc.sol
+++ contracts/BasicRateCalc.sol
@@ -23,10 +23,10 @@ contract BasicRateCalc is IRateCalc {
 
         //limit pool utilization (should never exceed 99% if amount is 0.5% of the pool)
         uint256 uP = oC.add(oP); //define pool utilization to be the sum of open calls and open puts
-        require(uP < tP/10, "pool is at maximum utilization"); //limit pool utilization to 10% of the pool
+        require(uP <= tP/10, "pool is at maximum utilization"); //limit pool utilization to 10% of the pool
 
         //check that option premium/payment is no more than 0.5% of the pool
-        require(amount < tP/200, "position too large");
+        require(amount <= tP/200, "position too large");
 
         uint256 canLock;
         //if the difference between calls and puts is zero
